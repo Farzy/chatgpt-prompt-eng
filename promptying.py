8f9be422f108cea1cdaf1f5918723a5f4e51ec8f
Farzad FARID <farzy@farzy.org>
2023-05-02 11:45:06 +0200

    Add one more manual test in Python script

--- promptying.py
+++ promptying.py
@@ -19,21 +19,30 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 
 
 text = f"""
-You should express what you want a model to do by \ 
-providing instructions that are as clear and \ 
-specific as you can possibly make them. \ 
-This will guide the model towards the desired output, \ 
-and reduce the chances of receiving irrelevant \ 
-or incorrect responses. Don't confuse writing a \ 
-clear prompt with writing a short prompt. \ 
-In many cases, longer prompts provide more clarity \ 
-and context for the model, which can lead to \ 
+You should express what you want a model to do by
+providing instructions that are as clear and
+specific as you can possibly make them.
+This will guide the model towards the desired output, 
+and reduce the chances of receiving irrelevant
+or incorrect responses. Don't confuse writing a
+clear prompt with writing a short prompt.
+In many cases, longer prompts provide more clarity
+and context for the model, which can lead to
 more detailed and relevant outputs.
 """
 prompt = f"""
-Summarize the text delimited by triple backticks \ 
+Summarize the text delimited by triple backticks
 into a single sentence.
 ```{text}```
+"""
+response = get_completion(prompt)
+print(response)
+
+prompt = f"""
+Generate a list of three made-up book titles along
+with their authors and genres. 
+Provide them in JSON format with the following keys: 
+book_id, title, author, genre.
 """
 response = get_completion(prompt)
 print(response)
